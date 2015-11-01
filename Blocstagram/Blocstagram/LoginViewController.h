2573305f923f4ae3961010412438d7f300f0fc24
Cody Rapp <codydoc@gmail.com>
2015-11-01 12:00:54 -0800

    checkpoint 33 completed

---
+++ Blocstagram/Blocstagram/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  Blocstagram
+//
+//  Created by Cody Rapp on 11/1/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+extern NSString *const LoginViewControllerDidGetAccessTokenNotification;
+
+@end
