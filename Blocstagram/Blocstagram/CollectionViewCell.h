7e73d752bb7a6800f6c96b1b11280798134f24f6
Cody Rapp <codydoc@gmail.com>
2015-12-13 23:03:44 -0800

    collectionview subclass added

---
+++ Blocstagram/Blocstagram/CollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.h
+//  Blocstagram
+//
+//  Created by Cody Rapp on 12/13/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Blocstagram/Blocstagram/CollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.m
+//  Blocstagram
+//
+//  Created by Cody Rapp on 12/13/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+@end
