acc13f2cd4b08b0503c70d3da65cc5ee97155b9e
Cody Rapp <codydoc@gmail.com>
2015-12-15 21:16:55 -0800

    assignment 43 complete

--- Blocstagram/Blocstagram/CollectionViewCell.m
+++ Blocstagram/Blocstagram/CollectionViewCell.m
@@ -11,29 +11,18 @@
 @implementation CollectionViewCell
 
 
--(id)init {
+-(id)initWithFrame:(CGRect)frame {
     
-    self = [super init];
+    self = [super initWithFrame:frame];
 
     if(self)
     {
-    
-        NSLog(@"You've initiatized subclass CollectionViewCell");
         
-        static NSInteger imageViewTag = 1000;
-        static NSInteger labelTag = 1001;
-        
-        self.thumbnail = (UIImageView *)[self.contentView viewWithTag:imageViewTag];
-        
-        self.label = (UILabel *)[self.contentView viewWithTag:labelTag];
-    
-        
-        CGFloat thumbnailEdgeSize = 158.00;
+            CGFloat thumbnailEdgeSize = 158.00;
 
         
             self.thumbnail = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, thumbnailEdgeSize, thumbnailEdgeSize)];
             self.thumbnail.contentMode = UIViewContentModeScaleAspectFill;
-            self.thumbnail.tag = imageViewTag;
             self.thumbnail.clipsToBounds = YES;
             
             [self.contentView addSubview:self.thumbnail];
@@ -41,7 +30,6 @@
         
         
             self.label = [[UILabel alloc] initWithFrame:CGRectMake(0, thumbnailEdgeSize, thumbnailEdgeSize, 20)];
-            self.label.tag = labelTag;
             self.label.textAlignment = NSTextAlignmentCenter;
             self.label.font = [UIFont fontWithName:@"HelveticaNeue-Medium" size:10];
             [self.contentView addSubview:self.label];
